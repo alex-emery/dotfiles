cc19ba9f5728bafd0ad534160d4f2ea49fd32439
alex emery <alex.emery@couchbase.com>
2022-09-06 14:05:08 +0100

    tidied up vimrc

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -3,19 +3,27 @@ return require('packer').startup(function()
 
     use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
     use 'nvim-treesitter/playground'
-    use { 'neovim/nvim-lspconfig', config = function() 
-        require 'plugins.nvim-lspconfig'
+    use { 'nvim-treesitter/nvim-treesitter-context', config = function()
+        require 'treesitter-context'.setup{}
     end }
 
-    -- Auto Completion
+    -- LSP and Friends
+    use { 'neovim/nvim-lspconfig', config = function()
+        require 'plugins.nvim-lspconfig'
+    end }
     use 'hrsh7th/cmp-nvim-lsp'
     use 'hrsh7th/cmp-vsnip'
     use 'hrsh7th/vim-vsnip'
 
-    use { 'hrsh7th/nvim-cmp', config = function() 
+    use { 'hrsh7th/nvim-cmp', config = function()
         require 'plugins.nvim-cmp'
     end }
 
+    use {'mfussenegger/nvim-lint', config = function()
+        require('lint').linters_by_ft = {
+            golang = {'golangcilint',}
+        }
+    end}
     -- Debugger
     use { 'mfussenegger/nvim-dap', config = function()
         require 'plugins.nvim-dap'
@@ -28,12 +36,13 @@ return require('packer').startup(function()
         require 'plugins.nvim-dap-virtual-text'
     end }
     use { "rcarriga/nvim-dap-ui", config = function()
-        require 'plugins.nvim-dap-ui' 
+        require 'plugins.nvim-dap-ui'
     end }
+
     -- fzf
     use {'junegunn/fzf', run = 'fzf#install()' }
     use 'junegunn/fzf.vim'
-    -- terescope 
+    -- terescope
     use {
         'nvim-telescope/telescope.nvim',
         requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}},
@@ -41,14 +50,15 @@ return require('packer').startup(function()
     }
     use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
     use {'nvim-telescope/telescope-dap.nvim' }
+
     -- Vim dispatch
     use { 'tpope/vim-dispatch' }
     -- Fugitive for Git
     use { 'tpope/vim-fugitive' }
     -- Seamless tmux navigation
     use 'christoomey/vim-tmux-navigator'
-    -- file explorer and icons
 
+    -- file explorer and icons
     use {
         'kyazdani42/nvim-tree.lua',
         requires = {
@@ -58,15 +68,11 @@ return require('packer').startup(function()
         tag = 'nightly' -- optional, updated every week. (see issue #1193)
     }
 
-    use {'rrethy/vim-hexokinase', run='make'}
+    -- use {'rrethy/vim-hexokinase', run='make'}
 
-    use {'mfussenegger/nvim-lint', 
-    config = function()
-        require('lint').linters_by_ft = {
-            golang = {'golangcilint',}
-        }
-    end
-}
+    -- themes
+    -- use 'folke/tokyonight.nvim'
+
 end)
 
 -- https://github.com/tomaskallup/dotfiles/tree/master/nvim/lua/plugins
